3b901988f1e40442c7cd34aa0e409206b18b1b66
loviyadav161 <alovish001@gmail.com>
2022-02-14 16:32:00 +0530

    worked to Cart Button on home page to add items from meals

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,25 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) =>{
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNo = +enteredAmount;
+        if(enteredAmount.trim().length === 0 || enteredAmount < 1 || enteredAmount > 5){
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNo);
+    };
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input = {{
+        <form className={classes.form} onSubmit = {submitHandler}>
+            <Input
+                ref = {amountInputRef}
+                label="Amount" input = {{
                 id: 'amount',
                 type: 'number',
                 min: '1',
@@ -13,6 +28,7 @@ const MealItemForm = (props) =>{
                 defaultValue: '1'
             }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please Enter a valid amount (1-5).</p>}
         </form>
     );
 };
