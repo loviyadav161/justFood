cefd72a5332ff8c37087ed97889d2ab73d991660
loviyadav161 <alovish001@gmail.com>
2022-02-14 22:39:47 +0530

    Added Fav Icon and Changed Title

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../Store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnHigh, setBtnHigh] = useState(false);
   const cartCtx = useContext(CartContext);  
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnHigh? classes.bump : ''}`;
+  useEffect(() => {
+      if(cartCtx.items.length === 0){
+          return;
+      }
+    setBtnHigh(true);
+    const timer = setTimeout(() => {
+        setBtnHigh(false);
+    },300);
+    return () => {
+        clearTimeout(timer);
+    };
+  }, [cartCtx.items])
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
